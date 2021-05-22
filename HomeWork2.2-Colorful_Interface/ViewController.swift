a4a15c54cf9f988e1d12d3747251313497327cf0
vstsiganow <slavok-48@mail.ru>
2021-05-22 20:40:07 +0300

    added full interface added functions in controller also added function "change alpha"

--- HomeWork2.2-Colorful_Interface/ViewController.swift
+++ HomeWork2.2-Colorful_Interface/ViewController.swift
@@ -13,11 +13,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var redColorLabel: UILabel!
     @IBOutlet weak var greenColorLabel: UILabel!
     @IBOutlet weak var blueColorLabel: UILabel!
+    @IBOutlet weak var alphaCoefficientLabel: UILabel!
     
     
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var alphaSlider: UISlider!
+    
     
     @IBOutlet weak var colorfulView: UIView!
     
@@ -26,16 +29,65 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         colorfulView.backgroundColor = backgroundViewColor
+        colorfulView.layer.cornerRadius = 20
+        colorfulView.layer.masksToBounds = true
+        
+        
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        redSlider.minimumTrackTintColor = rgbColor(red: 1, alpha: 0.9)
+        
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
+        greenSlider.minimumTrackTintColor = rgbColor(green: 1, alpha: 0.9)
+        
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 1
+        blueSlider.minimumTrackTintColor = rgbColor(blue: 1, alpha: 0.9)
+        
+        alphaSlider.minimumValue = 0
+        alphaSlider.maximumValue = 1
+        alphaSlider.minimumTrackTintColor = backgroundViewColor
         
     }
 
+    func rgbColor(red: Float = 0, green: Float = 0, blue: Float = 0, alpha: Float = 1) -> UIColor {
+        UIColor(red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue), alpha: CGFloat(alpha))
+    }
     
     private var backgroundViewColor: UIColor {
-        .red
+        rgbColor(red: redSlider.value, green: greenSlider.value, blue: blueSlider.value, alpha: alphaSlider.value)
+    }
+    
+    private func changedBackgroundColor() {
+        colorfulView.backgroundColor = backgroundViewColor
+        alphaSlider.minimumTrackTintColor = backgroundViewColor
     }
     
     
+    @IBAction func changedRedSliderValue() {
+        redColorLabel.text = String(format: "%.2f", redSlider.value)
+        changedBackgroundColor()
+    }
+    
+    @IBAction func changedGreenSliderValue() {
+        greenColorLabel.text = String(format: "%.2f", greenSlider.value)
+        changedBackgroundColor()
+    }
+    
+    @IBAction func changedBlueSliderValue() {
+        blueColorLabel.text = String(format: "%.2f", blueSlider.value)
+        changedBackgroundColor()
+    }
+    
+    @IBAction func changedAlphaSliderValue() {
+        alphaCoefficientLabel.text = String(format: "%.2f", alphaSlider.value)
+        changedBackgroundColor()
+        
+    }
+    
     
 
+    
 }
 
